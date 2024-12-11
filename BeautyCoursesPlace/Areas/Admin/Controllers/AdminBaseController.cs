70ef24990fe6a1861dcb0430ef730c049c1c3a80
Petya Deyanska <petya.krilcheva@abv.bg>
2024-12-11 12:06:26 +0200

    Admin

---
+++ BeautyCoursesPlace/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static BeautyCoursesPlace.Core.Constants.RoleConstants;
+
+namespace BeautyCoursesPlace.Areas.Admin.Controllers
+{
+    [Authorize(Roles = AdminRole)]
+    public class AdminBaseController : Controller
+    {
+       
+    }
+}
--- BeautyCoursesPlace/Program.cs
+++ BeautyCoursesPlace/Program.cs
@@ -55,13 +55,15 @@ app.UseEndpoints(endpoints =>
       defaults: new { Controller = "Course", Action = "Details" }
   );
 
+   
+
     endpoints.MapControllerRoute(
-            name: "areas",
-            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
+           name: "areas",
+           pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+   );
+
 
     endpoints.MapDefaultControllerRoute();
-
     endpoints.MapRazorPages();
 });
 
